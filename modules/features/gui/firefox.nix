5937e07292e5cd7984ec0a04b53f8f4e5a967a3d
SLH <git@slh.dev>
2026-04-21 01:56:53 +0200

    add vimium extension to firefox

--- modules/features/gui/firefox.nix
+++ modules/features/gui/firefox.nix
@@ -43,6 +43,11 @@
             install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
             private_browsing = true;
           };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            installation_mode = "force_installed";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+            private_browsing = true;
+          };
         };
         FirefoxHome = {
           Highlights = false;
